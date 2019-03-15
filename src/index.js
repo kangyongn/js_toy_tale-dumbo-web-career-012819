f9175988472c5561b350eef0174ecc55655a173e
github username <github email address>
2019-03-15 10:47:46 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
 
+const TOYURL = "http://localhost:3000/toys"
+
 // YOUR CODE HERE
+
+const toyDivTag = document.querySelector('#toy-collection');
+
+const toyCard = (toy) => {
+  return `<div class="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p data-toy-id="${toy.id}">${toy.likes} Likes </p>
+    <button class="like-btn">Like <3</button>
+  </div>`
+}
+const toys = () => {
+  fetch(TOYURL)
+  .then (resp => resp.json())
+  .then(toys => {
+    for (let toy of toys){
+      toyDivTag.innerHTML += toyCard(toy);
+    }
+  });
+}
+toys()
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
@@ -15,5 +38,7 @@ addBtn.addEventListener('click', () => {
   }
 })
 
-
 // OR HERE!
+
+
+// helper method.
